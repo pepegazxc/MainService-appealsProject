0ecf04d75f68e3c1a145d91a8936243ec3783ab0
pepegazxc <werdewrs555@gmail.com>
2026-02-24 15:56:44 +0300

    feature(security) : Added basic Spring Security and OAuth2 for JWT configuration

---
+++ src/main/java/main/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package main.security;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.SecretKey;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Value("${token.key}")
+    private String jwtSecret;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtDecoder jwtDecoder) throws Exception{
+        http.
+                csrf(token -> token.disable()).
+                authorizeHttpRequests(request -> request
+                        .requestMatchers("").hasRole("USER_ROLE")
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth -> oauth
+                        .jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(){
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
+        SecretKey spec = Keys.hmacShaKeyFor(keyBytes);
+        return NimbusJwtDecoder.withSecretKey(spec).build();
+    }
+}
+
+
