7031768a82d3832d01e526933394554f86b8c123
pepegazxc <werdewrs555@gmail.com>
2026-02-24 15:31:17 +0300

    global commit

---
+++ src/main/java/main/MainServiceAppealsProjectApplication.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainServiceAppealsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainServiceAppealsProjectApplication.class, args);
+    }
+
+}
