bb75369fe09739f172acb67dcb243fb9799d7652
pepegazxc <werdewrs555@gmail.com>
2026-02-28 14:57:24 +0300

    feature(service) : Implement method to save an appeal

--- src/main/java/main/controller/AppealsController.java
+++ src/main/java/main/controller/AppealsController.java
@@ -1,14 +1,22 @@
 package main.controller;
 
 import main.dto.request.UserRequest;
+import main.service.AppealsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppealsController {
 
+    private final AppealsService appealsService;
+
+    public AppealsController(AppealsService appealsService) {
+        this.appealsService = appealsService;
+    }
+
     @PostMapping("/write")
     public String writeAnAppeal(UserRequest request){
+        appealsService.saveAppeal(request);
         return "Your appeal has been submitted successfully!";
     }
 }
--- src/main/java/main/service/AppealsService.java
+++ src/main/java/main/service/AppealsService.java
@@ -1,7 +1,36 @@
 package main.service;
 
+import main.dto.request.UserRequest;
+import main.entity.AppealsEntity;
+import main.repository.AppealsRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AppealsService {
+
+    private final AppealsRepository appealsRepository;
+
+    public AppealsService(AppealsRepository appealsRepository) {
+        this.appealsRepository = appealsRepository;
+    }
+
+    public void saveAppeal(UserRequest request){
+        AppealsEntity appeals = buildAppeal(request);
+
+        appealsRepository.save(appeals);
+    }
+
+    private AppealsEntity buildAppeal(UserRequest request){
+        return AppealsEntity.builder()
+                .appeal(request.getAppeal())
+                .userIdentifier(getUserIdentifier())
+                .build();
+    }
+
+    private String getUserIdentifier(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return auth.getName();
+    }
 }
